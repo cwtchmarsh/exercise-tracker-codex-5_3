fd9843a685842ce83a4dba880d18d823e4fc86be
cwtchmarsh <harpoon-svelte-1l@icloud.com>
2026-02-21 16:45:09 -0500

    initial commit

---
+++ docs/assets/js/utils/sanitize.js
@@ -0,0 +1,9 @@
+export function escapeHtml(value) {
+    return value
+        .replaceAll("&", "&amp;")
+        .replaceAll("<", "&lt;")
+        .replaceAll(">", "&gt;")
+        .replaceAll('"', "&quot;")
+        .replaceAll("'", "&#39;");
+}
+//# sourceMappingURL=sanitize.js.map
